433783623d57423cb4b4caf7db27aa9563f7110f
mchtsz <mhatland206@gmail.com>
2022-11-21 15:24:34 +0100

    gym

---
+++ js/kropp.js
@@ -0,0 +1,40 @@
+const markdownElement = document.getElementById('markdowns') /// <div> hvor markdown text blir plasert
+const markdownList = document.getElementById('list') /// <div> liste for å navigere mellom til md på siden
+
+async function getFile(){
+    
+    const response = await fetch('https://raw.githubusercontent.com/mchtsz/markdown/main/konseptutv.%20og%20programmering/markdowns.txt') /// endre url til filens plassering
+    const text = await response.text()
+    const markdowns = text.trim().split('\n')
+    return markdowns
+}
+
+async function getMarkdowns(){
+    const markdowns = await getFile()
+    const responses = []
+
+    for(var filename of markdowns){
+        const response = await fetch(`https://raw.githubusercontent.com/mchtsz/markdown/main/konseptutv.%20og%20programmering/${filename}.md`) /// endre url til filens plassering men behold ${filename} delen
+        const text = await response.text()
+        responses.push({
+            text:text,
+            name:filename
+        })
+    }
+
+    return responses
+}
+
+async function showMarkdowns(){
+    const markdowns = await getMarkdowns()
+
+    for(var markdown of markdowns){
+        var html = drawdown(markdown.text)
+
+        markdownElement.innerHTML +=(`<div><a id="${markdown.name}"></a>${html}</div>`)
+        markdownList.innerHTML +=(`<a href="#${markdown.name}">${markdown.name}</a>`)
+    }
+    
+}
+
+showMarkdowns()
