b650b3df2c39aa3ce9dadac9bc040ab4dd25b7ca
mchtsz <mhatland206@gmail.com>
2022-12-12 12:54:00 +0100

    please funk

--- js/drawdown.js
+++ js/drawdown.js
@@ -4,7 +4,7 @@
  */
 
 
-;function drawdown(src) {
+; function drawdown(src) {
 
     var rx_lt = /</g;
     var rx_gt = />/g;
@@ -34,33 +34,33 @@
     }
 
     function blockquote(src) {
-        return src.replace(rx_blockquote, function(all, content) {
+        return src.replace(rx_blockquote, function (all, content) {
             return element('blockquote', blockquote(highlight(content.replace(/^ *&gt; */gm, ''))));
         });
     }
 
     function list(src) {
-        return src.replace(rx_list, function(all, ind, ol, num, low, content) {
+        return src.replace(rx_list, function (all, ind, ol, num, low, content) {
             var entry = element('li', highlight(content.split(
                 RegExp('\n ?' + ind + '(?:(?:\\d+|[a-zA-Z])[.)]|[*\\-+]) +', 'g')).map(list).join('</li><li>')));
 
             return '\n' + (ol
                 ? '<ol start="' + (num
                     ? ol + '">'
-                    : parseInt(ol,36) - 9 + '" style="list-style-type:' + (low ? 'low' : 'upp') + 'er-alpha">') + entry + '</ol>'
+                    : parseInt(ol, 36) - 9 + '" style="list-style-type:' + (low ? 'low' : 'upp') + 'er-alpha">') + entry + '</ol>'
                 : element('ul', entry));
         });
     }
 
     function highlight(src) {
-        return src.replace(rx_highlight, function(all, _, p1, emp, sub, sup, small, big, p2, content) {
+        return src.replace(rx_highlight, function (all, _, p1, emp, sub, sup, small, big, p2, content) {
             return _ + element(
-                  emp ? (p2 ? 'strong' : 'em')
-                : sub ? (p2 ? 's' : 'sub')
-                : sup ? 'sup'
-                : small ? 'small'
-                : big ? 'big'
-                : 'code',
+                emp ? (p2 ? 'strong' : 'em')
+                    : sub ? (p2 ? 's' : 'sub')
+                        : sup ? 'sup'
+                            : small ? 'small'
+                                : big ? 'big'
+                                    : 'code',
                 highlight(content));
         });
     }
@@ -87,13 +87,13 @@
     replace(rx_listjoin, '');
 
     // code
-    replace(rx_code, function(all, p1, p2, p3, p4) {
-        stash[--si] = element('pre', element('code', p3||p4.replace(/^    /gm, '')));
+    replace(rx_code, function (all, p1, p2, p3, p4) {
+        stash[--si] = element('pre', element('code', p3 || p4.replace(/^    /gm, '')));
         return si + '\uf8ff';
     });
 
     // link or image
-    replace(rx_link, function(all, p1, p2, p3, p4, p5, p6) {
+    replace(rx_link, function (all, p1, p2, p3, p4, p5, p6) {
         stash[--si] = p4
             ? p2
                 ? '<img src="' + p4 + '" alt="' + p3 + '"/>'
@@ -103,11 +103,11 @@
     });
 
     // table
-    replace(rx_table, function(all, table) {
+    replace(rx_table, function (all, table) {
         var sep = table.match(rx_thead)[1];
         return '\n' + element('table',
-            table.replace(rx_row, function(row, ri) {
-                return row == sep ? '' : element('tr', row.replace(rx_cell, function(all, cell, ci) {
+            table.replace(rx_row, function (row, ri) {
+                return row == sep ? '' : element('tr', row.replace(rx_cell, function (all, cell, ci) {
                     return ci ? element(sep && !ri ? 'th' : 'td', unesc(highlight(cell || ''))) : ''
                 }))
             })
@@ -115,13 +115,13 @@
     });
 
     // heading
-    replace(rx_heading, function(all, _, p1, p2) { return _ + element('h' + p1.length, unesc(highlight(p2))) });
+    replace(rx_heading, function (all, _, p1, p2) { return _ + element('h' + p1.length, unesc(highlight(p2))) });
 
     // paragraph
-    replace(rx_para, function(all, content) { return element('p', unesc(highlight(content))) });
+    replace(rx_para, function (all, content) { return element('p', unesc(highlight(content))) });
 
     // stash
-    replace(rx_stash, function(all) { return stash[parseInt(all)] });
+    replace(rx_stash, function (all) { return stash[parseInt(all)] });
 
     return src.trim();
 };
